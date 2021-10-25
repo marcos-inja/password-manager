b5566a02b028d80c7bed4b377b753ccd34e498be
marcos-vas <marcosvaas@gmail.com>
2021-10-24 21:48:57 -0300

    style: names and header csv

--- passwords.py
+++ passwords.py
@@ -6,6 +6,7 @@ import pandas as pd
 import os
 import pathlib
 
+
 class Passwords:
     def __init__(self, args):
         home_folder = os.getenv('HOME')
@@ -14,46 +15,45 @@ class Passwords:
         self.path_passwords = f'{home_folder}/.pss/senhas_servicos.csv'
         self.master_password = f'{home_folder}/.pss/master_ps.txt'
 
-    def run(self, funcao, msg='cadastre uma senha usando pss new'):
+    def run(self, function, msg='Cadastre uma senha usando $pss new'):
         if isfile(self.master_password):
             with open(self.master_password, 'r') as f:
-                password = input("pass >> ")
+                password = input('pass >> ')
                 r = hashlib.md5(password.encode())
                 ps = f.read()
                 ps2 = r.hexdigest()
-
                 if ps == ps2:
-                    funcao(password)
+                    function(password)
         else:
             print(msg)
 
     def new(self):
         if not isfile(self.master_password):
-            password = input("pass >> ")
+            password = input('pass >> ')
             with open(self.master_password, 'w') as ps:
                 r = hashlib.md5(password.encode())
                 ps.write(r.hexdigest())
 
     def cadastra_senha(self, password):
-        name_service = input("Nome >> ")
-        user_service = input("User >> ")
-        pass_service = input("Senha >> ")
+        name_service = input('service >> ')
+        user_service = input('user >> ')
+        pass_service = input('password >> ')
         if not isfile(self.path_passwords):
             with open(self.path_passwords, 'w') as f:
                 w = csv.writer(f)
-                w.writerow(['Nome', 'User', 'Senha'])
+                w.writerow(['Name', 'User', 'Pass'])
 
         with open(self.path_passwords, 'a') as ps:
             escrever = csv.writer(ps)
-            escrever.writerow([name_service, user_service, encrypt(pass_service, password)])
-        
+            escrever.writerow([name_service, user_service,
+                              encrypt(pass_service, password)])
 
-    def lista_senhas(self, password):  
+    def lista_senhas(self, password):
         df = pd.read_csv(self.path_passwords)
         for _, row in df.iterrows():
             print(
-                f"Nome: {row['Nome']} | Usuario: {row['User']} | Senha: {decrypt(row['Senha'], password)}")
-        
+                f"Nome: {row['Name']} | Usuario: {row['User']} | Senha: {decrypt(row['Pass'], password)}"
+            )
 
     def remove(self, password):
         with open(self.path_passwords, 'r') as f:
@@ -64,19 +64,18 @@ class Passwords:
             writer = csv.writer(f)
             ident = None
             for line in lst:
-                nome, usuario, senha = line
+                name, _, _ = line
 
-                if nome == self.args.nome:
-                    ident = nome
+                if name == self.args.nome:
+                    ident = name
 
-                if ident != nome:
+                if ident != name:
                     writer.writerow(line)
-       
 
     def encotra_por_chave(self, password):
         df = pd.read_csv(self.path_passwords)
         for _, row in df.iterrows():
-            if self.args.nome == row['Nome']:
+            if self.args.nome == row['Name']:
                 print(
-                    f"Nome: {row['Nome']} | Usuario: {row['User']} | Senha: {decrypt(row['Senha'], password)}")
-        
+                    f"Nome: {row['Name']} | Usuario: {row['User']} | Senha: {decrypt(row['Pass'], password)}"
+                )

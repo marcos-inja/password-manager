6beb3fef81fcd109818803bcc5bf1eb056f71071
marcos-vas <marcosvaas@gmail.com>
2021-10-28 20:42:06 -0300

    fix: keys changed to english

--- pss.py
+++ pss.py
@@ -6,37 +6,37 @@ HELP = """
 [ 1 - criar senha na primeira execução:  pss new                        ]
 [ 2 - cadatrar uma nova senha:           pss a                          ]
 [ 3 - listar serviços salvos:            pss ls                         ]
-[ 4 - remover senha salva:               pss r -ha <hash> ou -n <nome>  ]
-[ 5 - recuperar por hash ou nome:        pss ss -h <hash> ou -n <nome>  ]
+[ 4 - remover senha salva:               pss r -ha <hash> or -n <name>  ]
+[ 5 - recuperar por hash ou nome:        pss ss -h <hash> or -n <name>  ]
 """
 
 def main():
-    parser = argparse.ArgumentParser(description='gerenciador hehe')
-    parser.add_argument('chave', help=HELP)
-    parser.add_argument('-n', '--nome', required=False)
+    parser = argparse.ArgumentParser(description='KISS password manager')
+    parser.add_argument('key', help=HELP)
+    parser.add_argument('-n', '--name', required=False)
     parser.add_argument('-ha', '--hash', required=False)
     args = parser.parse_args()
 
-    funcoes = Passwords(args)
+    passwords = Passwords(args)
 
-    if args.chave == 'new':
-        funcoes.new()
+    if args.key == 'new':
+        passwords.new()
 
     # Cadastra nova senha
-    elif args.chave == 'a':
-        funcoes.run(funcoes.cadastra_senha)
+    elif args.key == 'a':
+        passwords.run(passwords.cadastra_senha)
 
     # Lista senhas salvas
-    elif args.chave == 'ls':
-        funcoes.run(funcoes.lista_senhas, 'Nothing registered yet')
+    elif args.key == 'ls':
+        passwords.run(passwords.lista_senhas, 'Nothing registered yet')
 
     # Remove uma senha por palavra chave
-    elif args.chave == 'r':
-        funcoes.run(funcoes.remove, 'Nothing registered yet')
+    elif args.key == 'r':
+        passwords.run(passwords.remove, 'Nothing registered yet')
 
     # Recupera uma unica senha
-    elif args.chave == 'ss':
-        funcoes.run(funcoes.encotra_por_chave, 'Nothing registered yet')
+    elif args.key == 'ss':
+        passwords.run(passwords.encotra_por_chave, 'Nothing registered yet')
 
     else:
         sys.exit('Flag not find!')
